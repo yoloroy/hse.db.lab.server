2eca1847137f1d5ae18904fc6865f21c5b07fcdf
yoloroy <slart2108@gmail.com>
2022-05-20 22:07:26 +0300

    :recycle: refactor observing booking events

--- src/model/BookingService.kt
+++ src/model/BookingService.kt
@@ -4,26 +4,31 @@ import com.yoloroy.domain.booking.BookingModel
 import com.yoloroy.domain.booking.BookingRepository
 import com.yoloroy.lib.extensions.isIntersect
 import com.yoloroy.lib.util.ResultOf
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filter
 import kotlinx.datetime.LocalDateTime
 
 class BookingService(private val bookingRepository: BookingRepository) {
     private val observersInDuration = mutableListOf<Pair<ClosedRange<LocalDateTime>, () -> Unit>>()
 
-    fun add(clientId: Int, tableId: Int, start: LocalDateTime, end: LocalDateTime) =
-        bookingRepository.add(clientId, tableId, start, end).also { println(it) }.also {
-            observersInDuration
-                .filter { (interval, _) -> (start..end) isIntersect interval }
-                .forEach { (_, action) -> action() }
+    fun add(clientId: Int, tableId: Int, start: LocalDateTime, end: LocalDateTime): ResultOf<BookingModel> =
+        bookingRepository.add(clientId, tableId, start, end).also {
+            notifyInInterval(start, end)
         }
 
     fun cancel(bookingId: Int, clientId: Int): ResultOf<BookingModel> =
         bookingRepository.cancel(bookingId, clientId).also {
-            (it as? ResultOf.Success)?.also { println(it) }?.value?.let { booking ->
-                observersInDuration
-                    .filter { (interval, _) -> (booking.start..booking.end) isIntersect interval }
-                    .forEach { (_, action) -> action() }
-            }
+            (it as? ResultOf.Success)?.value
+                ?.let { booking -> booking.start to booking.end }
+                ?.let { (start, end) -> notifyInInterval(start, end) }
         }
+
+    private fun notifyInInterval(start: LocalDateTime, end: LocalDateTime) {
+        observersInDuration
+            .filter { (interval, _) -> (start..end) isIntersect interval }
+            .forEach { (_, action) -> action() }
+    }
 
     fun observeInterval(interval: ClosedRange<LocalDateTime>, block: () -> Unit) = observersInDuration.add((interval to block))
 }
